04a6186f029d75cd9202a9bd170ee21139ea75f9
DavidSanwald <david.sanwald@gmail.com>
2019-06-10 23:31:42 +0200

    yay

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Chart from "./Chart";
+import { tap } from "ramda";
+import data from "./data.json";
+import "./App.css";
+
+const peek = tap(x => console.log(x));
+peek(data);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ position: "relative" }} className="App">
+      <Chart data={data} />
     </div>
   );
 }
